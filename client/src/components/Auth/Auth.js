ba7ea7a94f1472b13b79958c703ab1150ada0041
kishor choithani <choithanisunit@gmail.com>
2020-08-13 18:14:33 +0530

    dashboard ui

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { format } from "morgan";
+import "./Auth.scss";
+import twitterlogo from "./sign_in_twitter.png";
+import toplogo from "./landing-page.png";
 class Auth extends React.Component {
   constructor(props) {
     super(props);
     this.state = { dataState: null };
   }
   render() {
+    let lout = document.getElementById("logout");
+    if (lout) {
+      document.getElementById("home_page").style.backgroundColor = "black";
+      //document.getElementById("home_page").style.position = "fixed";
+      lout.style.backgroundColor = "black";
+      lout.style.display = "block";
+      lout.style.position = "fixed";
+    }
     return (
       <div id="home_page">
         {this.state.dataState}
@@ -34,9 +45,11 @@ class Auth extends React.Component {
         console.log(data);
         if (data) {
           let ds = (
-            <a id="logout" href="http://localhost:5000/logout">
-              Log Out
-            </a>
+            <div className="logout_block">
+              <a id="logout" href="http://localhost:5000/logout">
+                Log Out
+              </a>
+            </div>
           );
           let userinfo = Object.values(data)[0].profile;
           this.setState({ dataState: ds });
@@ -44,9 +57,17 @@ class Auth extends React.Component {
           this.props.infoHandler(userinfo);
         } else {
           let ds = (
-            <a id="login" href="http://localhost:5000/login">
-              Log In
-            </a>
+            <div id="login_div" href="http://localhost:5000/login">
+              <div>
+                <img id="toplogo" src={toplogo} alt="toplogo" />
+              </div>
+              <div>
+                <a id="login" href="http://localhost:5000/login">
+                  <img id="twitterlogo" src={twitterlogo} alt="twitter" />
+                </a>
+              </div>
+              <div className="login_text">Lets get your account set up!</div>
+            </div>
           );
           this.setState({ dataState: ds });
         }
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -10,37 +10,77 @@ class Dashboard extends React.Component {
     this.state = { queue: null };
   }
   render() {
+    var btns = document.getElementsByClassName("sidenav_div");
+
+    // Loop through the buttons and add the active class to the current/clicked button
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+
+        // If there's no active class
+        if (current.length > 0) {
+          current[0].className = current[0].className.replace(" active", "");
+        }
+
+        // Add the active class to the current/clicked button
+        this.className += " active";
+      });
+    }
     return (
       <div
         id="dashboard"
         style={{ display: this.props.showDash ? "block" : "none" }}
       >
-        <div className="navbar">
-          <div>PostIt</div>
-
-          {this.props.info ? (
-            <div>Welcome {this.props.info.displayName}</div>
-          ) : (
+        <div className="navbar_fixed">
+          <div className="navbar">
             <div></div>
-          )}
-        </div>
-        {this.props.info.username ? (
-          <div className="tl">
-            <Tl name={this.props.info.username}></Tl>
+            <div className="brand_name">Post.It.</div>
+
+            {this.props.info ? (
+              <div id="navbar_inner_flex">
+                <div>Welcome {this.props.info.displayName} </div>
+                {this.props.info._json ? (
+                  <div id="profile_pic_div">
+                    <img
+                      id="profile_pic"
+                      src={this.props.info._json.profile_image_url_https}
+                      alt="profile_pic"
+                    />
+                  </div>
+                ) : null}
+              </div>
+            ) : (
+              <div></div>
+            )}
           </div>
-        ) : null}
+        </div>
+        <div className="body">
+          <div className="sidenav">
+            <div className="sidenav_div">1</div>
+            <div className="sidenav_div">2</div>
+            <div className="sidenav_div">3</div>
+            <div className="sidenav_div">4</div>
+          </div>
+          <div className="main">
+            {this.props.info.username ? (
+              <div className="tl">
+                <Tl name={this.props.info.username}></Tl>
+              </div>
+            ) : null}
 
-        {this.state.queue ? (
-          <Queue
-            forceQueue={this.forceQueue}
-            info={this.props.info}
-            queue={this.state.queue}
-          ></Queue>
-        ) : (
-          <div>Queue is empty</div>
-        )}
+            {this.state.queue ? (
+              <Queue
+                forceQueue={this.forceQueue}
+                info={this.props.info}
+                queue={this.state.queue}
+              ></Queue>
+            ) : (
+              <div>Queue is empty</div>
+            )}
 
-        <Date info={this.props.info} forceQueue={this.forceQueue}></Date>
+            <Date info={this.props.info} forceQueue={this.forceQueue}></Date>
+          </div>
+        </div>
       </div>
     );
   }
